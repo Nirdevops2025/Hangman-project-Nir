47f8aa2cc4f4fdd417291375640c023d5b0bb05e
Nir Cohen <nir.cohen64@gmail.com>
2025-11-09 21:41:27 +0200

    add get input

--- main_game.py
+++ main_game.py
@@ -1,2 +1,15 @@
-print ("welcome to Hangman Game")
-print ("new text")
+def get_input():
+    ltr=input("Enter your letter: ")
+    ltr=ltr.lower()
+    if len(ltr)==1:
+        if ltr.isalpha():
+            return ltr
+        else:
+            ltr = False
+            return ltr
+    else:
+        ltr = False
+        return ltr
+
+if __name__=='__main__':
+    get_input()
